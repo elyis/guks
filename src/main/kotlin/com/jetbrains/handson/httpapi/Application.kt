5ae72cc403f9dd165bc1a0460cf785e552b421cb
elyi <elyi7367@gmail.com>
2022-04-01 10:00:11 +0500

    выгрузки фото и токенов нет

--- src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
@@ -1,10 +1,32 @@
 package com.jetbrains.handson.httpapi
 
-import io.ktor.application.Application
-
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.gson.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import java.io.File
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
+    install(ContentNegotiation)
+    {
+        gson()
+    }
 
+    routing {
+        Authentication()
+        Catalog()
+        get{
+            val file = File("index.jpeg")
+            call.response.header(
+                HttpHeaders.ContentDisposition,
+                ContentDisposition.Attachment.withParameter(ContentDisposition.Parameters.FileName, "index.jpeg")
+                    .toString()
+            )
+            call.respondFile(file)
+        }
+    }
 }
